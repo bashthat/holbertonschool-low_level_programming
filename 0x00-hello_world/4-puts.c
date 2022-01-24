0aeaf7fb49898e38add1a640db356758c0c64369
bashthat <earthstar412@gmail.com>
2022-01-24 17:36:34 +0000

    betty-is-cleared

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /* this is a c program in style..  */
-int main(void)
+int main(void);
+/*main always returns 0*/
 {
 puts("Programming is like building a multilingual puzzle\n");
 return (0);
