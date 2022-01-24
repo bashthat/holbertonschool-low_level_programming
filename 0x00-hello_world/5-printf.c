76f981aee580f9169e3a62cfe3f173d321210976
bashthat <earthstar412@gmail.com>
2022-01-24 19:31:57 +0000

    sNOwWhite

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-/* this is all betty styled work */
+/**
+ *this is all betty styled wor
+ *main - this is a printf funcioned program
+ *Return: always void
+ */
 int main(void)
-/* the function main returns 0 */
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
