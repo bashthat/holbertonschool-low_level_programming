918ed9eb1ebec834461f93763c790e0a5148151d
bashthat <earthstar412@gmail.com>
2022-01-24 19:55:03 +0000

    %types%

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+/**
+ * main - functions on types
+ * Return: always zero
+ */
 main()
+/* functions for the sizeof%Types% */
 {
-
+printf("%lu\n", sizeof(char));
+printf("%lu\n", sizeof(int));
+printf("%lu\n", sizeof(float));
+printf("%lu", sizeof(double));
+return (0);
 }
