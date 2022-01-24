11910811018d00fef178326b4b4a09c446445737
bashthat <earthstar412@gmail.com>
2022-01-24 20:00:09 +0000

    longint

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,8 +7,9 @@ main()
 /* functions for the sizeof%Types% */
 {
 printf("%lu\n", sizeof(char));
-printf("%lu\n", sizeof(int));
+printf("%lu\n", sizeof(long int));
 printf("%lu\n", sizeof(float));
 printf("%lu", sizeof(double));
+printf("%lu", sizeof(long long int));
 return (0);
 }
