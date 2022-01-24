ef46bd452acf249b64fbc6abf27e5d876025a31d
bashthat <earthstar412@gmail.com>
2022-01-24 17:40:25 +0000

    syntax

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /* this is a c program in style..  */
-int main(void);
+int main(void)
 /*main always returns zero*/
 {
 puts("Programming is like building a multilingual puzzle\n");
