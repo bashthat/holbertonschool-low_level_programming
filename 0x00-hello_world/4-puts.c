03fd1de8f1937e0877395a9690fc61c10aa7a7e4
bashthat <earthstar412@gmail.com>
2022-01-24 18:54:40 +0000

    whitespace

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
-/*
- * this is a c program in style..
- */
+/*this is a c program in style..*/
 int main(void)
-/* 
- * the function main always returns zero
- */
+/* the function main always returns zero*/
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
