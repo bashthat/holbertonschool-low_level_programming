00d545f2e8fe8cabba57c997e571053abfbd653f
bashthat <earthstar412@gmail.com>
2022-01-24 17:16:27 +0000

    roger that betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#this is a c program in style.. woot -- learned lesson of the day, don't over think it!
+/* this is a c program in style.. woot -- learned lesson of the day, don't over think it! */
 int main(void)
 {
 puts ("Programming is like building a multilingual puzzle\n");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,5 @@
-#!/bin/bash
 #include stdio.h
-main()
+/*this is all betty styled work*/
+main(void)
 printf ("with proper grammar, but the outcome is a piece of art,\n");
 return 0;
