484a37c91d0cab0827d949e142dcebc010e83c6e
bashthat <earthstar412@gmail.com>
2022-01-24 17:20:10 +0000

    bluezy

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-/* this is a c program in style.. woot -- learned lesson of the day, don't over think it! */
+/* this is a c program in style..  */
 int main(void)
 {
-puts ("Programming is like building a multilingual puzzle\n");
-return 0;
+puts("Programming is like building a multilingual puzzle\n");
+return (0);
 }
