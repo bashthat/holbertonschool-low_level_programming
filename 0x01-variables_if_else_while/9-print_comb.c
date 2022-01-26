f10ea878580917f42951031bbf0942a5104be1a0
bashthat <earthstar412@gmail.com>
2022-01-26 00:36:31 +0000

    tryagain

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - this is a printf 0-9 script
+ *Return: zero
+ */
+int main(void)
+{
+int a;
+for (a = '0'; a <= '9'; a++)
+{
+putchar(a);
+putchar(',');
+}
+putchar('\n');
+return (0);
+}
