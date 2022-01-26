4d35289d29b421a40f12d9bee99fb87a96b28c66
bashthat <earthstar412@gmail.com>
2022-01-26 20:04:54 +0000

    testing

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
-#include <stdio.h>
+#include "holberton.h"
 /* this is a betty styled this format in c coding */
 /**
  *main - this is a printf script
  *Return: zero
  */
-int main(void)
+int main(void);
 {
-printf("_putchar\n");
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
 return (0);
 }
