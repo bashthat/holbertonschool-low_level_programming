01451901a75549a9878017a9e5a4afcfb50529eb
bashthat <earthstar412@gmail.com>
2022-01-27 00:42:16 +0000

    edits

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-/*includes*/
-
-#include <unistd.h>
-#include <stdio.h>
-
-/*prototypes*/
-
-int_putchar(char c);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -20,5 +12,4 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-
 #endif/*MAIN_H*/
