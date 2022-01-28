5dced18b0809739b9853674e1b4bd1f0b3483831
bashthat <earthstar412@gmail.com>
2022-01-28 01:06:32 +0000

    nothing is perfect

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *main - this is a program
+ *Return: always zero
+ *_isalpha: code check
+ */
+int _isalpha(int c)
+/*check the code always zero*/
+{
+if
+((c >= 'a' && c <= 'z')) || ((c >= 'A' && c <= 'Z'));
+{
+return (1);
+else
+return (0);
+}
+}
