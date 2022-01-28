e736f54fe56b36332c6682d304a69cb61936eb23
root <3249@holbertonschool.com>
2022-01-27 21:47:17 -0800

    tenz

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main: the function adds x and z
- * @add - adds the integers x and z
+ * main: the function adds @x and @z
+ * add - adds the integers x and z
  * x - an integer to the equation.
  * z - the other integer of the equation
- * Return: always zero
+ * Return: always zero or one
  */
 /*this is a program that is simple, a+b, in this case z and x*/
 int add(int x, int z)
