30311fea8c31a30fa4f91c4843508255672dc544
bashthat <earthstar412@gmail.com>
2022-01-28 01:34:50 +0000

    art

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main: the function adds x and z
+ * @add - adds the integers x and z
+ * @x - an integer to the equation.
+ * @z - the other integer of the equation
+ *
+ */
+int add(int x, int z)
+{
+return ( x + z );
+}
