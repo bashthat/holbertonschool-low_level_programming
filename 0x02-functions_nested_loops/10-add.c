a957aa37916218b928c6efa01710150e8758a292
bashthat <earthstar412@gmail.com>
2022-01-28 01:39:05 +0000

    addz

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,11 +3,13 @@
 /**
  * main: the function adds x and z
  * @add - adds the integers x and z
- * @x - an integer to the equation.
- * @z - the other integer of the equation
+ * x - an integer to the equation.
+ * z - the other integer of the equation
  * Return: always zero
  */
+/*this is a program that is simple, a+b, in this case z and x*/
 int add(int x, int z)
 {
+/*according to betty x and z are members... but of what?*/
 return (x + z);
 }
