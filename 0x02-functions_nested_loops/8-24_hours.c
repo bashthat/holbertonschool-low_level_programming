2d07e39d80ff38d0c9e3222cf2050ce2e7605242
root <3249@holbertonschool.com>
2022-01-28 19:10:28 -0800

    jackbauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *jack_bauer - what time is it?
+ *@hours- integer for hours.
+ *@minutes- integer for minutes
+ */
+void jack_bauer(void)
+{
+int hours, minutes;
+hours = 0;
+{
+while (hours < 24)
+minutes = 0;
+while (minutes < 60)
+{
+putchar((hours / 10) == '0');
+putchar((hours % 10) + '0');
+putchar((':'));
+putchar((minutes / 10) == '0');
+putchar((minutes % 10) == '0');
+putchar('\n');
+}
+minutes++;
+hours++;
+}
+}
