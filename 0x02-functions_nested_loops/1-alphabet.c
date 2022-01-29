2dddbe757948f21a62a8696f8aa5d4f32aadcefb
root <3249@holbertonschool.com>
2022-01-28 17:31:29 -0800

    noodles

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,9 @@
 #include "main.h"
-/* this is a betty styled this format in c coding */
+
 /**
- *main - print_alphabet prototype with putchar
  *Return: zero
- *ft_print_alphabet - prints alphabet as a prototype
- *make coding fun.. zz+top.
+ *@_print_alphabet - prints alphabet as a prototype to the main functioon
+ *@p the character at hand, may the stage be presented as the alphabet!
  */
 
 void print_alphabet(void)
