85c012a3ff19d8ea14df633f7e2b8780007c9ff2
root <3249@holbertonschool.com>
2022-02-01 11:39:55 -0800

    bettysyntax

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * @c - returns 1 if uppercase, returns 0 if lowercase.
+ * _isupper - checks for uppercase letters.
+ * @c - returns 1 if uppercase, returns 0 if lowercase, integer of above function..
  */
 
 int _isupper(int c)
