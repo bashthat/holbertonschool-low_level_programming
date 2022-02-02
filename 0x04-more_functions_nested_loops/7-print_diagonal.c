da27543561e7a5234085a1e41df6f1ea49a02472
root <3249@holbertonschool.com>
2022-02-01 22:08:44 -0800

    dagwood

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_diagonal - prints a diagonal expression by design
+ * @n: integer of truth
+ * Return: multiples of a and b.
+ */
+void print_diagonal(int n)
+{
+int s;
+int a;
+
+if (n > 0)
+{
+for (a = 0; a < n; a++)
+{
+for (s = 0; s <= a; s++)
+{
+if (s != 1)
+_putchar(' ');
+if (s == a)
+{
+_putchar('\\');
+_putchar('\n');
+}
+}
+}
+}
+else
+{
+_putchar('\n');
+}
+}
