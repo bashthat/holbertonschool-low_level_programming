aa8d943bef7db3412c0f66b8c38265dfba50f676
root <3249@holbertonschool.com>
2022-02-01 19:37:45 -0800

    print

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_numbers - printst integers 0 through 9
+ *@x: first integer
+ *_putchar: prints numbers
+ */
+
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x < = 9; x++)
+	{
+		_putchar('0' + x);
+	}
+	_putchar('\n');
+}
