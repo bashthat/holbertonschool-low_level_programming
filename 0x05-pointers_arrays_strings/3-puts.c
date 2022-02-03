a12a125457b1a5756d95202eab9f6c0ec3fad24b
root <3249@holbertonschool.com>
2022-02-02 16:44:26 -0800

    worwd

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_puts - prints violin stringz
+ *@str: charecter
+ *Return: zero
+ */
+void _puts(char *str)
+{
+int x;
+for
+(x = 0; str[x] != '\0'; x++);
+{
+_putchar(str[x]);
+}
+_putchar('\n');
+}
