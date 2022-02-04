c943a077ed3cef392db641670581e736f31ff712
root <3249@holbertonschool.com>
2022-02-03 21:21:36 -0800

    betty edits

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,13 +8,13 @@ void print_rev(char *s)
 {
 int x = 0;
 
-while (s[x] != 0)
+while (*(s + x))
 x++;
-x--;
+x = x - 1;
 while (x >= 0)
-x--;
+
 {
-_putchar(s[x]);
+_putchar(*(s + x));
 x--;
 }
 _putchar('\n');
