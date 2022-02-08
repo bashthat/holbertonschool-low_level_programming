14a9175ac24815bc5a94487b532e2c5c5b4e37f6
root <3249@holbertonschool.com>
2022-02-07 20:52:51 -0800

    slops

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,28 +2,22 @@
 /**
  * _strcmp - main function to compare strings (s1, s2)
  * @s1: first string
- * @s2: second string 
+ * @s2: second string
  * Return: returns to destination
  */
-int *_strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
-int x, diff;
+int x;
 
 x = 0;
 
-while (s2[x] != '\0' || s1[x] != '\0')
+while (s1[x] != '\0' && s2[x] != '\0')
 {
-if (s2[x] != s1[x])
+if (s1[x] != s2[x])
 {
-diff = s1[x] - s2[x];
-break;
+return (s1[x] - s2[x]);
 }
-else
-{
-diff = 0;
+x++;
 }
-s1++;
-s2++;
-}
-return (diff);
+return (0);
 }
