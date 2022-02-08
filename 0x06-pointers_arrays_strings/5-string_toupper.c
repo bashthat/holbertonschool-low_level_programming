90f2fba4af98abca8cd1869a39b9920098047012
root <3249@holbertonschool.com>
2022-02-07 21:04:05 -0800

    updates_lack_of_integer

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,15 +6,16 @@
  */
 char *string_toupper(char *)
 {
-int x = 0;
+int x;
 
+x = 0;
 while (s[x] != '\0')
 {
+
 if (s(x) >= 97 && s[x] <= 122)
-{
-s(x) = (int)s[x] - 32;
-}
+s[x] = s[x] -32;
 x++;
+
 }
 return (0);
 }
