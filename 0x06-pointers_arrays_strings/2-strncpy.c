fcefdad8a5d62c91aaef39bfae7e66e742a5c4fc
root <3249@holbertonschool.com>
2022-02-07 17:45:56 -0800

    gcc

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * _strncpy - main function.
+ * @dest: destination of the copy
+ * @src: source
+ * @n: the integer used for function.
+ * Return: returns to destination
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int x;
+
+x = 0;
+while (src[x] != 0 && x < n)
+{
+dest[x] = src[x];
+x++;
+}
+while (x < n)
+{
+dest[x] = '\0';
+x++;
+}
+return (dest);
+}
