d9abbc0bfe62d236a8d1191ab7bd1f9b7a688b6d
root <3249@holbertonschool.com>
2022-02-12 10:17:18 -0800

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -4,19 +4,20 @@
  * 
  * Return: always zero
  */
-char *leet(char *z)
+char *leet(char *v)
 {
 int x, y;
-char m7[] = "aAeEoOtTlL";
-char m5[] = "4433007711";
+char m7[] = "AEOTL";
+char m6[] = "aeotl";
+char m5[] = "43071";
 
-for (x = 0; z[x] != '\0'; x++)
+for (x = 0; v[x] != '\0'; x++)
 {
 for (y = 0; y < 10; y++)
 {
-	if (z[x] == m7[y])
+	if (v[x] == m7[y])
 	{
-	z[x] = m5[y];
+	v[x] = m5[y];
 	}
 }
 	return (0);
