3ce64db5b4c02668e9a5c98145a1c6cf8fded7d2
root <3249@holbertonschool.com>
2022-02-14 18:22:46 -0800

    chmod

--- 0x0A-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x0A-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 gcc -Wall -Werror -Wextra -pedantic -std=gnu89 *.c -c -fPIC
 gcc -Wall -Werror -Wextra -pedantic -std=gnu89 *.o -shared -o liball.so
+export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH
