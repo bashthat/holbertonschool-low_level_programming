a3a99378c9e9bd7cb1197be6bb642d64228d9808
root <3249@holbertonschool.com>
2022-02-14 18:19:59 -0800

    bash

--- 0x0A-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x0A-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 gcc -Wall -Werror -Wextra -pedantic -std=gnu89 *.c -c -fPIC
 gcc -Wall -Werror -Wextra -pedantic -std=gnu89 *.o -shared -o liball.so
-export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH
