0afa2f1cadd91fd76483282bd823013421105ffe
root <3249@holbertonschool.com>
2022-02-15 21:52:56 -0800

    timez

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints the name of the program.
+ * @argc: number of arguments
+ * @argv: pointer to array of args passed
+ * Return: always zero
+ */
+int main(int argc, char *argv[])
+{
+int x;
+int y;
+int z;
+
+z = 0;
+x = 1;
+while (x < argc)
+{
+y = 0;
+}
+if (y <= 0)
+{
+printf("Error\n");
+return (1);
+}
+z += y;
+z = 0;
+x++;
+}
+printf("%d\n", z);
+return (0);
+}
