46c730ddc646e73ed779dae217297fe60c71bda0
root <3249@holbertonschool.com>
2022-02-17 17:42:48 -0800

    doubleheaderz

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef MOCK_ABS_H
+#define MOCK_ABS_H
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,4 @@
+#ifndef MOCK_SUM_H
+#define MOCK_SUM_H
+#define SUM(x, y) ((x) + (y))
+#endif /* SUM(x, y) */
