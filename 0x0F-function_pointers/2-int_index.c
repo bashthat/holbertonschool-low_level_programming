6f5299206cc31959375c3a195bbe9938b598870d
root <3249@holbertonschool.com>
2022-02-23 21:00:01 -0800

    array

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,16 +10,18 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 
-	int x = 0;
-	int result = 0;
+	int x, result;	
 
-if (array == NULL || cmp == NULL || size <= 0)
+if (array == NULL || cmp == NULL)
 	return (-1);
 
-	for (x = 0; x < size; x++)
+if (size <= 0)
+	return (-1);
+
+for (x = 0; x < size; x++)
 	{
-		result == cmp(*(array + x));
-		if (result != 0)
+		result == cmp(array[x]);
+		if (result == 1)
 			return (x);
 }
 	return (-1);
