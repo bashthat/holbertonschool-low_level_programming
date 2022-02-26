9f595f9b65f07298677bad20c3614562c145e57f
root <3249@holbertonschool.com>
2022-02-25 22:02:28 -0800

    lastcall

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,20 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_strings - main function
+ * @n: const int
+ * @separator: separates
+ * Return: always void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+unsigned int y;
+char *string;
+
+va_list list
+
+return (0);
+}
+
+
