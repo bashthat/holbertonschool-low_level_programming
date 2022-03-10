c40c67399ca773be3bbf978de6d19fd4463db6a9
bashthat <3249@holbertonschool.com>
2022-03-10 06:44:08 -0800

    updated

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,15 +6,15 @@
  * Return: the node count.
  */
 
-size_t print_listint(const listint_t *h);
+size_t print_listint(const listint_t *h)
 {
-        unsigned int i = 0;
+	unsigned int i = 0;
 
-        while (h != NULL)
-        {
-                printf("%d\n", h->n);
-                h = h->next;
-                i++;
-        }
-        return (i);
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
