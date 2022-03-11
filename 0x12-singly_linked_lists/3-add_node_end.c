6401e8de093d4db4fa3f8b01e93eb46376cdc620
Sam <earthstar412@gmail.com>
2022-03-11 05:59:19 +0000

    not in time

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * add_node_end - adds node at end of string
+ * @head: points sto NULL
+ * @str: stsring
+ * Return: length
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+}
---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * free_lits - free lits
+ * head: pointer
+ * Return: just in time
+ */
+void free_list(list_t *head)
+{
+	return (0);
+}
