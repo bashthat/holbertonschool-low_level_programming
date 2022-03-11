e0feb7536b9f2b22734f7e214ce4a9df68ce1765
Sam <earthstar412@gmail.com>
2022-03-11 05:19:01 +0000

    update

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all elementents given in a list
+ * @h: pointer to a node
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned int i = 0;
+	const list_t *tmp = NULL;
+	
+	tmp = h;
+	
+	while (tmp != NULL)
+	{
+		if (tmp->str == NULL)
+		{
+		printf("[0] (nil)\n");
+		}
+		
+		else
+		
+			printf("[%d] %s\n", tmp->len, tmp->str);
+		tmp = tmp->next;
+		i++;
+	}
+	return (i);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+these are singly linked lists that we went over on the Pld day
