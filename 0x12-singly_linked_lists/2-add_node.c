71adee9dcd49870e8208ec9ae147db68beada7ef
bashthat <3249@holbertonschool.com>
2022-03-14 16:03:52 -0700

    betty

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,29 +1,37 @@
 #include "lists.h"
-
+#include <string.h>
 /**
  * add_node: adds node.
  * @head: pointer of strct
  * @str: *string
  * Return: address of string
  */
-
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *tmp;
-	unsigned int counter = 0;
+	char *d;
+	int len;
 
-	tmp = malloc(sizeof(list_t));
-	if (tmp == NULL)
+	list_t *new
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 		return (NULL);
 
-	tmp->str = strdup(str);
-	
-	while (str[counter] != '/0')
+	d = strdup(str);
+	if (d == NULL)
 	{
-		counter++;
+		free(new);
+		return (NULL);
 	}
-	tmp->len = counter;
-	tmp->next = *head;
-	*head = tmp;
-	return (tmp);
+
+	for (len = 0; str[len];)
+		len++;
+
+	new->str = d;
+	new->len = len;
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
 }
