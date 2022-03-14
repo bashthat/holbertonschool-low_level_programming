b78b926ef3c33f781e14d55d6b8e46396c6ad779
bashthat <3249@holbertonschool.com>
2022-03-14 16:41:59 -0700

    edits

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,16 +1,15 @@
 #include "lists.h"
 /**
- * free_lits - free lits
- * head: pointer
+ * free_list - free lits
+ * @head: pointer
  * Return: just in time
  */
 void free_list(list_t *head)
 {
-while (head != NULL)
-	{
-		free(head->str);
-		free(head);
-		head = head->next;
-	}
-
+if (head == NULL)
+return;
+if (head->next != NULL)
+free_list(head->next);
+free(head->str);
+free(head);
 }
