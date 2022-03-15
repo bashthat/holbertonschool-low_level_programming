1086be172a91ef54ecb94154a7ebf5dbd18def12
bashthat <3249@holbertonschool.com>
2022-03-15 16:36:10 -0700

    bettygibberish

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	xy = open(filename, O_CREAT | O_WRONLY | O_TRUNC | S_IWUSR);
 	if (xy == -1)
 		return (-1);
-	
+
 	if (text_content)
 	{
 		for (i = 0; text_content[i] != '\0'; i++)
@@ -25,7 +25,6 @@ int create_file(const char *filename, char *text_content)
 			if (star == -1)
 				return (-1);
 	}
-	
 	close(xy);
 	return (1);
 }
