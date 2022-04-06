8f2e5e2c7e8871aeff5500d7423161c9f4963cf6
bashthat <3249@holbertonschool.com>
2022-04-06 14:31:14 -0700

    bettystatus

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - adds to the table
+ * @key: key value
+ * @ht: hash_table status
+ * @value: key value
+ * Return: one success, zero unsuccessful.
+ */
+
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+
+}
