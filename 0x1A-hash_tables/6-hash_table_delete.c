c444d3fadc82d39ceb6d0f65b42c96b5c51279be
bashthat <3249@holbertonschool.com>
2022-04-06 16:05:58 -0700

    thoughts

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * @hash_table_delete - deletes hash table
+ * @ht: hash table
+ * Return: NULL
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned int z = 0;
+	hash_node_t *node = NULL;
+
+	for (z = 0; z < ht->size; z++)
+	{
+	while (ht->array[z] != NULL)
+	{
+		node = ht->array[z]->next;
+		free(ht->array[z]->key);
+		free(ht->array[z]->value);
+		free(ht->array[z]);
+		ht->array[z] = node;
+	}
+	}
+	free(ht->array);
+	free(ht);
+}
