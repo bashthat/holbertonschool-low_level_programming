5594ec5eb6136a912ab8703ec7b3ffe51d6dfb18
bashthat <3249@holbertonschool.com>
2022-04-06 14:58:21 -0700

    edits

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,3 +1,5 @@
+#include <string.h>
+#include <stdlib.h>
 #include "hash_tables.h"
 /**
  * hash_table_set - adds to the table
@@ -10,5 +12,26 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	unsigned long indx;
+	unsigned long sz;
+	hash_node_t *vim = NULL;
+
+	if (ht == NULL || key == NULL || value == NULL)
+		return (0);
+
+	vim = malloc(sizeof(hash_node_t));
+	if (vim == NULL)
+		return (0);
+
+	indx = key_index((const unsigned char *)key, sz);
+	sz = ht->size;
+
+	
+	vim->key = strdup(key);
+	vim->value = strdup(value);
+	vim->next = ht->array[indx];
+	ht->array[indx] = vim;
+
+	return (1);
 
 }
