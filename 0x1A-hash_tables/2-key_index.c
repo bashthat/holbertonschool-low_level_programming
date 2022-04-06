02cf8072ec7ffc1a58ca032c6d1bc3ee24954dc2
bashthat <3249@holbertonschool.com>
2022-04-06 14:12:10 -0700

    checkingerrors

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+/**
+ * key_index - computes the key given to the hash_table.
+ * @key: what key is given to the table?
+ * @size: size of the hash table.
+ * Return: hash_djb2(key) function % size.
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
