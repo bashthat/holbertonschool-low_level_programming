c3e667c3b5ff54b1afa86daba3bc8ab7eac5883d
Sam <earthstar412@gmail.com>
2022-04-07 03:54:26 +0000

    edits

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * hash_table_print - prints table
+ * @ht: hash tabla
+ * Return: null
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *index;
+	unsigned long i;
+	int j;
+
+	if (ht == NULL)
+	return;
+
+	for (i = 0; i <= ht->size; i++)
+	{
+	if (ht->array[i])
+	{
+	index = ht->array[i];
+	
+	for (j = 0; index != NULL; index = index->next, j++)
+	{
+	if (j == 0)
+	printf(" ");
+	printf("%s': '%s'", index->key, index->value);
+	}
+	}
+	}
+
+}
