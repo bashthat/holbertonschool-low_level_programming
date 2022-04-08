38a497fae94a63c03ba65f238d840f3ac8a90ecb
Sam <earthstar412@gmail.com>
2022-04-08 04:56:14 +0000

    edits

--- 0x18-doubly_linked_lists/5-get_dnodeint.c
+++ 0x18-doubly_linked_lists/5-get_dnodeint.c
@@ -10,12 +10,19 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int z = 0;
 
-	for (; z < index && head; z++)
-	{
-		head = head->next;
-	}
-	if (head != NULL)
+	dlistint_t *temp = head;
+
+	if (!head)
 		return (NULL);
 
-	return (head);
+	while (temp != NULL)
+	{
+		if (z == index)
+		return (temp);
+
+	temp = temp->next;
+	z++;
+	}
+	
+	return (NULL);
 }
