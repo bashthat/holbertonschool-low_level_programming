1a174fc0dd51fe32370ff2d17aa67d6c389e0a01
Sam <earthstar412@gmail.com>
2022-04-08 05:37:38 +0000

    edits

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,23 +1,7 @@
 #include "hash_tables.h"
 #include <stdio.h>
 #include <stdlib.h>
-/**
- * free_list - function frees
- * @head: this is the hash_node_t list
- */
-void free_list(hash_node_t *head)
-{
-	hash_node_t *lint;
-	while (lint)
-	
-	{
-	lint = head->next;
-	free(lint->key);
-	free(lint->value);
-	free(lint);
-	head = lint;
-	}
-}
+
 /**
  * hash_table_delete - deletes hash table
  * @ht: hash table
@@ -25,14 +9,25 @@ void free_list(hash_node_t *head)
  */
 void hash_table_delete(hash_table_t *ht)
 {
+	hash_node_t *lint, *tmp;
 	unsigned int x = 0;
 	
 	if (!ht)
 	return;
 	
 	for (; x < ht->size; x++)
-	
-	free_list(ht->array[x]);
+		{
+		lint = ht->array[x];
+		while (lint)
+		{
+	tmp = lint;
+	free(tmp->key);
+	free(tmp->value);
+	lint = lint->next;
+	free(tmp);
+		}
+	free(lint);
+	}
 	free(ht->array);
 	free(ht);
 }
