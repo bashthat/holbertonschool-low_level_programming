c2588317997e49fb0b35ad594e28aa3021009a1b
bashthat <3249@holbertonschool.com>
2022-07-21 13:51:04 -0500

    space

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,26 +8,21 @@
 
 size_t print_list(const list_t *h)
 {
-
-  int x = 0;
-  if (h == NULL)
-    return (0);
-
-  while (h != NULL)
-  {
-    if (h->str == NULL)
-    {
-      printf("[%d] %s\n", 0, "(nil)");
-    }
-    else
-    {
-      printf("[%d] %s\n", h->len, h->str);
-    }
-    h = h->next;
-    x++;
-  }
-
-  return (x);
-
+int x = 0;
+if (h == NULL)
+return (0);
+while (h != NULL)
+{
+if (h->str == NULL)
+{
+printf("[%d] %s\n", 0, "(nil)");
 }
-
+else
+{
+printf("[%d] %s\n", h->len, h->str);
+}
+h = h->next;
+x++;
+}
+return (x);
+}
