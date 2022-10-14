90de1963105e62a1114bc5fe6ae4fb48e2af1849
bashthat <3249@holbertonschool.com>
2022-10-14 14:45:16 -0500

    betty is still here to hauntme.md

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -9,27 +9,26 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-    size_t i, j, k, m;
-    int n;
-
-    if (array == NULL)
-        return (-1);
-    i = 0;
-    j = size - 1;
-    while (i <= j)
-    {
-        printf("Searching in array: ");
-        for (k = i; k < j; k++)
-            printf("%d, ", array[k]);
-        printf("%d\n", array[k]);
-        m = (i + j) / 2;
-        n = array[m];
-        if (n < value)
-            i = m + 1;
-        else if (n > value)
-            j = m - 1;
-        else
-            return (m);
-    }
-    return (-1);
+size_t i, j, k, m;
+int n;
+if (array == NULL)
+return (-1);
+i = 0;
+j = size - 1;
+while (i <= j)
+{
+printf("Searching in array: ");
+for (k = i; k < j; k++)
+printf("%d, ", array[k]);
+printf("%d\n", array[k]);
+m = (i + j) / 2;
+n = array[m];
+if (n < value)
+i = m + 1;
+else if (n > value)
+j = m - 1;
+else
+return (m);
+}
+return (-1);
 }
