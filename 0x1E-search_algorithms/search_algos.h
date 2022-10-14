9bb82deca8d9d65cdf09b4d18e931daea4736bdd
bashthat <3249@holbertonschool.com>
2022-10-14 15:01:14 -0500

    typos, edits, and syntax

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,8 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
+// C programming libraries
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+// prototypes and functions
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-#endif
+// spacing
+
+#endif // MAIN_H
