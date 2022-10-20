6b0370dc6b9a074785a17b4a8ad897ba5b0e67ae
SamAlberts <97474996+bashthat@users.noreply.github.com>
2022-10-20 13:45:59 -0500

    kerenel errors

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -8,41 +8,38 @@
 **/
 int advanced_binary(int *array, size_t size, int value)
 {
-    size_t xyz = 0;
-    size_t i = 0;
-    int zyx = 0;
-    int index;
-
-    if (array == NULL)
-        return (-1);
-    printf("Searching in array: ");
-    for (xyz = 0; xyz < size; xyz++)
-    {
-        printf("%d", array[xyz]);
-        if (xyz < size - 1)
-            printf(", ");
-    }
-    i = (size - 1) / 2;
-    if (array[i] == value && array[i - 1] != value)
-    {
-        printf("\n");
-        return (i);
-    }
-    if (array[i] >= value)
-    {
-        printf("\n");
-        return (advanced_binary(array, i + 1, value));
-    }
-    if (array[i] < value)
-    {
-        printf("\n");
-        zyx = advanced_binary(array + i + 1, size - i - 1, value);
-        if (zyx == -1)
-            return (-1);
-        index = zyx + i + 1;
-        return (index);
-    }
-    return (-1);
+size_t xyz = 0;
+size_t i = 0;
+int zyx = 0;
+int index;
+if (array == NULL)
+return (-1);
+printf("Searching in array: ");
+for (xyz = 0; xyz < size; xyz++)
+{
+printf("%d", array[xyz]);
+if (xyz < size - 1)
+printf(", ");
 }
-
-    
+i = (size - 1) / 2;
+if (array[i] == value && array[i - 1] != value)
+{
+printf("\n");
+return (i);
+}
+if (array[i] >= value)
+{
+printf("\n");
+return (advanced_binary(array, i + 1, value));
+}
+if (array[i] < value)
+{
+printf("\n");
+zyx = advanced_binary(array + i + 1, size - i - 1, value);
+if (zyx == -1)
+return (-1);
+index = zyx + i + 1;
+return (index);
+}
+return (-1);
+}
