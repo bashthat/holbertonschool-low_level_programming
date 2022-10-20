110e50f4e8a1af793d3db4df8109036c100f8761
SamAlberts <97474996+bashthat@users.noreply.github.com>
2022-10-20 14:08:54 -0500

    whitespace

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,13 +1,13 @@
 #include "search_algos.h"
-
 /**
- *advanced_binary - searches for a value in a sorted array of integers
- *@array: points to the first element of the array
- *@size: number of elements in array
- *@value: value to search
- *Return: index where value is located
- **/
-int advanced_binary(int * array, size_t size, int value) {
+ * advanced_binary - searches for a value in a sorted array of integers
+ * @array: points to the first element of the array
+ * @size: number of elements in array
+ * @value: value to search
+ * Return: index where value is located
+**/
+int advanced_binary(int * array, size_t size, int value)
+{
    size_t xyz = 0;
    size_t i = 0;
    int zyx = 0;
@@ -15,21 +15,25 @@ int advanced_binary(int * array, size_t size, int value) {
    if (array == NULL)
       return (-1);
    printf("Searching in array: ");
-   for (xyz = 0; xyz < size; xyz++) {
+   for (xyz = 0; xyz < size; xyz++)
+   {
       printf("%d", array[xyz]);
       if (xyz < size - 1)
          printf(", ");
    }
    i = (size - 1) / 2;
-   if (array[i] == value && array[i - 1] != value) {
+   if (array[i] == value && array[i - 1] != value)
+   {
       printf("\n");
       return (i);
    }
-   if (array[i] >= value) {
+   if (array[i] >= value)
+   {
       printf("\n");
       return (advanced_binary(array, i + 1, value));
    }
-   if (array[i] < value) {
+   if (array[i] < value)
+   {
       printf("\n");
       zyx = advanced_binary(array + i + 1, size - i - 1, value);
       if (zyx == -1)
