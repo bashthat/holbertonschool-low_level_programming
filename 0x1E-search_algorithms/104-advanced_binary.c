2163d93347ce72f33355dc5bb75ff19034fb39b6
SamAlberts <97474996+bashthat@users.noreply.github.com>
2022-10-20 13:39:12 -0500

    newline

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -11,50 +11,38 @@ int advanced_binary(int *array, size_t size, int value)
     size_t xyz = 0;
     size_t i = 0;
     int zyx = 0;
-    int index = 0;
+    int index;
 
     if (array == NULL)
         return (-1);
-    if (size == 1 && array[0] != value)
-        return (-1);
-    xyz = size / 2;
-    if (array[xyz] == value)
+    printf("Searching in array: ");
+    for (xyz = 0; xyz < size; xyz++)
     {
-        if (array[xyz - 1] == value)
-        {
-            for (i = xyz; i > 0; i--)
-            {
-                if (array[i] != value)
-                    return (i + 1);
-            }
-        }
-        return (xyz);
+        printf("%d", array[xyz]);
+        if (xyz < size - 1)
+            printf(", ");
     }
-    if (array[xyz] > value)
+    i = (size - 1) / 2;
+    if (array[i] == value && array[i - 1] != value)
     {
-        printf("Searching in array: ");
-        for (i = 0; i < xyz; i++)
-        {
-            printf("%d, ", array[i]);
-        }
-        printf("Found %d at index: %d\n", array[xyz], (int)xyz);
-        zyx = advanced_binary(array, xyz, value);
+        printf("\n");
+        return (i);
+    }
+    if (array[i] >= value)
+    {
+        printf("\n");
+        return (advanced_binary(array, i + 1, value));
+    }
+    if (array[i] < value)
+    {
+        printf("\n");
+        zyx = advanced_binary(array + i + 1, size - i - 1, value);
         if (zyx == -1)
             return (-1);
-        index = zyx;
+        index = zyx + i + 1;
+        return (index);
     }
-    else
-    {
-        printf("Searching in array: ");
-        for (i = xyz; i < size; i++)
-        {
-            printf("%d, ", array[i]);
-        }
-        printf("Found %d at index: %d\n", array[xyz], (int)xyz);
-        zyx = advanced_binary(array + xyz, size - xyz, value);
-        if (zyx == -1)
-            return (-1);
-        index = zyx + xyz;
-    }
-    return (index);
+    return (-1);
 }
+
+    
