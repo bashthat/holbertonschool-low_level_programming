7f876f7f150a3223a9eac25f5407447cdaad1930
SamAlberts <97474996+bashthat@users.noreply.github.com>
2022-10-25 12:13:06 -0500

    git changes

---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,50 @@
+#include "search_algos.h"
+
+/**
+ * linear_skip - searches for a value given a linear skip.
+ * @list: list points to the head of the list.
+ * @value: value the program searches for.
+ * Return: The value
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+    skiplist_t *tmp = NULL;
+
+    if (list == NULL)
+        return (NULL);
+
+    tmp = list->express;
+    while (tmp != NULL)
+    {
+        printf("Value checked at index [%lu] = [%d] \n", tmp->index, tmp->n);
+
+        if (tmp->n >= value)
+        {
+            printf("Value found between indexes [%lu] and [%lu] \n",
+                   list->index, tmp->index);
+            break;
+        }
+
+        list = tmp;
+        tmp = tmp->express;
+    }
+
+    if (tmp == NULL)
+    {
+        tmp = list;
+        while (tmp->next != NULL)
+            tmp = tmp->next;
+        printf("Value found between indexes [%lu] and [%lu] \n",
+               list->index, tmp->index);
+    }
+
+    while (list != NULL && list->n <= value)
+    {
+        printf("Value checked at index [%lu] = [%d] \n", list->index, list->n);
+        if (list->n == value)
+            return (list);
+        list = list->next;
+    }
+
+    return (NULL);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,5 +1,6 @@
 #ifndef __SEARCH_ALGOS_H__
 #define __SEARCH_ALGOS_H__
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -25,6 +26,28 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ */
+typedef struct skiplist_s
+{
+    int n;
+    size_t index;
+    struct skiplist_s *next;
+    struct skiplist_s *express;
+} skiplist_t;
+
+
+// linear skip list
+skiplist_t *linear_skip(skiplist_t *list, int value);
+// linear search
 int advanced_binary(int *array, size_t size, int value);
 
 #endif /* __SEARCH_ALGOS_H__ */
