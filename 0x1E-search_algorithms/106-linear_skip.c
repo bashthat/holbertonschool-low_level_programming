b85d988d8ef4effd338e8fe112cd72b78de4932f
bashthat <3249@holbertonstudents.com>
2022-10-25 17:42:31 +0000

    whitespace

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -8,43 +8,43 @@
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-    skiplist_t *tmp = NULL;
+skiplist_t *tmp = NULL;
 
-    if (list == NULL)
-        return (NULL);
+if (list == NULL)
+return (NULL);
 
-    tmp = list->express;
-    while (tmp != NULL)
-    {
-        printf("Value checked at index [%lu] = [%d]\n", tmp->index, tmp->n);
+tmp = list->express;
+while (tmp != NULL)
+{
+printf("Value checked at index [%lu] = [%d]\n", tmp->index, tmp->n);
 
-        if (tmp->n >= value)
-        {
-            printf("Value found between indexes [%lu] and [%lu]\n",
-                   list->index, tmp->index);
-            break;
-        }
+if (tmp->n >= value)
+{
+printf("Value found between indexes [%lu] and [%lu]\n",
+list->index, tmp->index);
+break;
+}
 
-        list = tmp;
-        tmp = tmp->express;
-    }
+list = tmp;
+tmp = tmp->express;
+}
 
-    if (tmp == NULL)
-    {
-        tmp = list;
-        while (tmp->next != NULL)
-            tmp = tmp->next;
-        printf("Value found between indexes [%lu] and [%lu]\n",
-               list->index, tmp->index);
-    }
+if (tmp == NULL)
+{
+tmp = list;
+while (tmp->next != NULL)
+tmp = tmp->next;
+printf("Value found between indexes [%lu] and [%lu]\n",
+list->index, tmp->index);
+}
 
-    while (list != NULL && list->n <= value)
-    {
-        printf("Value checked at index [%lu] = [%d]\n", list->index, list->n);
-        if (list->n == value)
-            return (list);
-        list = list->next;
-    }
+while (list != NULL && list->n <= value)
+{
+printf("Value checked at index [%lu] = [%d]\n", list->index, list->n);
+if (list->n == value)
+return (list);
+list = list->next;
+}
 
-    return (NULL);
+return (NULL);
 }
